5fb4845a8dff2e01206c4f3641dfe2fdef4db1ff
kenichi <xianyigoben53@gmail.com>
2022-07-25 16:29:47 +0900

    投稿ページの作成

--- blog_create.php
+++ blog_create.php
@@ -1,4 +1,41 @@
 <?php
 
-$blog = $_POST;
-var_dump($blog);
+require_once("dbConnect.php");
+use Blog\Dbc;
+
+$blogs = $_POST;
+
+if(empty($blogs['title'])){
+    exit('タイトルを入力してください');
+}else if(mb_strlen($blogs['title']) > 191){
+    exit('タイトルは191文字以内にしてください');
+}
+if(empty($blogs['content'])){
+    exit('本文を入力してください');
+}
+if(empty($blogs['category'])){
+    exit('カテゴリーを選択してください');
+}
+if(empty($blogs['publish_status'])){
+    exit('公開ステータスを選択してください');
+}
+
+$dbh = Dbc\dbConnect();
+$dbh->beginTransaction();
+$sql = 'INSERT INTO 
+            blog(title, content, category, publish_status)
+        VALUES
+            (:title, :content, :category, :publish_status)';
+try{
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':title', $blogs['title'], PDO::PARAM_STR);
+    $stmt->bindValue(':content', $blogs['content'], PDO::PARAM_STR);
+    $stmt->bindValue(':category', $blogs['category'], PDO::PARAM_INT);
+    $stmt->bindValue(':publish_status', $blogs['publish_status'], PDO::PARAM_INT);
+    $stmt->execute();
+    $dbh->commit();
+    echo '投稿されました';
+} catch(PDOException $e) {
+    $dbh->rollBack();
+    exit($e);
+}
